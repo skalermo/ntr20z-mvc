8c17921315f0f8ae4026124f0212237b15ac6ce3
Roman Moskalenko <skalermo@gmail.com>
2020-10-28 14:29:46 +0100

    Pass data from links to modal form

--- SchoolScheduler/wwwroot/js/site.js
+++ SchoolScheduler/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var a = $(event.relatedTarget) // Button that triggered the modal
+
+    var room = a.data('room') // Extract info from data-* attributes
+    var group = a.data('group')
+    var klass = a.data('class')
+    var teacher = a.data('teacher')
+
+    var modal = $(this)
+    modal.find('.modal-title').text('something different')
+    modal.find('input[id=room]').val(room)
+    modal.find('input[id=group]').val(group)
+    modal.find('input[id=class]').val(klass)
+    modal.find('input[id=teacher]').val(teacher)
+})
