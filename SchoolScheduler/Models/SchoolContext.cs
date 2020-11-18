f2bfdd76019f8816115fe6a7f06cd503d1a5abde
Roman Moskalenko <skalermo@gmail.com>
2020-11-18 11:06:58 +0100

    Group db initialization in functions

--- SchoolScheduler/Models/SchoolContext.cs
+++ SchoolScheduler/Models/SchoolContext.cs
@@ -8,6 +8,7 @@ public class SchoolContext : DbContext
     public DbSet<Subject> Subjects { get; set; }
     public DbSet<Teacher> Teachers { get; set; }
     public DbSet<Activity> Activities { get; set; }
+    public DbSet<Slot> Slots { get; set; }
 
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -19,6 +20,12 @@ public class SchoolContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        InitialMigration(modelBuilder);
+        PopulateMigration(modelBuilder);
+    }
+
+    private void InitialMigration(ModelBuilder mb)
+    {
         string[] days = { "Mo", "Tu", "We", "Th", "Fr" };
         string[] times = { "8:00-8:45", "8:55-9:40", "9:50-11:35",
         "11:55-12:40", "12:50-13:55", "13:45-14:30",
@@ -29,8 +36,42 @@ public class SchoolContext : DbContext
         {
             foreach (var day in days)
             {
-                modelBuilder.Entity<Slot>().HasData(new Slot() { SlotId = i++, Name = day + " " + time });
+                mb.Entity<Slot>().HasData(new Slot() { SlotId = i++, Name = day + " " + time });
             }
         }
+    }
+
+    private void PopulateMigration(ModelBuilder mb)
+    {
+        string[] roomNames = { "110", "111", "120", "121" };
+        int i = 1;
+        foreach (var name in roomNames)
+        {
+            mb.Entity<Room>().HasData(new Room() { Id = i++, Name = name });
+        }
+
+        string[] classGroupNames = { "1a", "1b", "1c", "2a", "2b", "3a", "3b", "4a", "4b" };
+        i = 1;
+        foreach (var name in classGroupNames)
+        {
+            mb.Entity<ClassGroup>().HasData(new ClassGroup() { Id = i++, Name = name });
+        }
+
+        string[] subjectNames = { "mat", "geo", "eng", "phys", "biol" };
+        i = 1;
+        foreach (var name in subjectNames)
+        {
+            mb.Entity<Subject>().HasData(new Subject() { Id = i++, Name = name });
+        }
+
+        string[] teacherNames = { "kowalski", "nowak", "smith", "clarkson", "may", "hammond", "atkinson" };
+        i = 1;
+        foreach (var name in teacherNames)
+        {
+            mb.Entity<Teacher>().HasData(new Teacher() { Id = i++, Name = name });
+        }
+
+        mb.Entity<Activity>().HasData(new Activity() { ActivityId = 1, RoomId = 1, ClassGroupId = 1, SubjectId = 1, SlotId = 1, TeacherId = 1 });
+        mb.Entity<Activity>().HasData(new Activity() { ActivityId = 2, RoomId = 4, ClassGroupId = 3, SubjectId = 3, SlotId = 3, TeacherId = 2 });
     }
 }
