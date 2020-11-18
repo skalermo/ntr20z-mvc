65a4879aeaf07600970e99ffef5a462ff0c77b94
Roman Moskalenko <skalermo@gmail.com>
2020-11-18 11:05:53 +0100

    Override ToString method

--- SchoolScheduler/Models/Entity.cs
+++ SchoolScheduler/Models/Entity.cs
@@ -7,5 +7,10 @@ namespace SchoolScheduler.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public virtual List<Activity> Activities { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
