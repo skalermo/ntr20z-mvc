8b5f9dee3ec90f4d7f319b53326dd1bb35ba0b76
Roman Moskalenko <skalermo@gmail.com>
2020-11-18 21:19:50 +0100

    Add migrations

---
+++ SchoolScheduler/Migrations/20201118082654_populate.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolScheduler.Migrations
+{
+    public partial class populate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ClassGroups",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "1a" },
+                    { 2, "1b" },
+                    { 3, "1c" },
+                    { 4, "2a" },
+                    { 5, "2b" },
+                    { 6, "3a" },
+                    { 7, "3b" },
+                    { 8, "4a" },
+                    { 9, "4b" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 3, "120" },
+                    { 4, "121" },
+                    { 1, "110" },
+                    { 2, "111" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Subjects",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "mat" },
+                    { 2, "geo" },
+                    { 3, "eng" },
+                    { 4, "phys" },
+                    { 5, "biol" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 6, "hammond" },
+                    { 1, "kowalski" },
+                    { 2, "nowak" },
+                    { 3, "smith" },
+                    { 4, "clarkson" },
+                    { 5, "may" },
+                    { 7, "atkinson" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Activities",
+                columns: new[] { "ActivityId", "ClassGroupId", "RoomId", "SlotId", "SubjectId", "TeacherId" },
+                values: new object[] { 1, 1, 1, 1, 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Activities",
+                columns: new[] { "ActivityId", "ClassGroupId", "RoomId", "SlotId", "SubjectId", "TeacherId" },
+                values: new object[] { 2, 3, 4, 3, 3, 2 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Activities",
+                keyColumn: "ActivityId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Activities",
+                keyColumn: "ActivityId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ClassGroups",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ClassGroups",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ClassGroups",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "ClassGroups",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "ClassGroups",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "ClassGroups",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "ClassGroups",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Subjects",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Subjects",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Subjects",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "ClassGroups",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ClassGroups",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Subjects",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Subjects",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ SchoolScheduler/Migrations/20201118201220_timestamp.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace SchoolScheduler.Migrations
+{
+    public partial class timestamp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "Teachers",
+                rowVersion: true,
+                nullable: true)
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "Subjects",
+                rowVersion: true,
+                nullable: true)
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "Slots",
+                rowVersion: true,
+                nullable: true)
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "Rooms",
+                rowVersion: true,
+                nullable: true)
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "ClassGroups",
+                rowVersion: true,
+                nullable: true)
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "Activities",
+                rowVersion: true,
+                nullable: true)
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.ComputedColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "Subjects");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "Slots");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "ClassGroups");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "Activities");
+        }
+    }
+}
